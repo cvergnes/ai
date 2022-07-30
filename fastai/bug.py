c61a38216e338ae72bd6968a6d295c3fc99ab65d
cvergnes <cvergnes@gmail.com>
2022-07-30 13:23:22 +0200

    WIP

--- fastai/bug.py
+++ fastai/bug.py
@@ -1,5 +1,5 @@
 from fastai.vision.utils import *
 from pathlib import Path
 
-path = Path("bmw-750.jpeg")
-resize_images( file=path, dest=path) 
+path = Path("images")
+resize_images(path, dest=path) 
