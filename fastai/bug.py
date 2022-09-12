614e87713140e0a1321fa8ce75ebab0fa95bd835
cvergnes <cvergnes@gmail.com>
2022-09-12 21:21:30 +0200

    wip

--- fastai/bug.py
+++ fastai/bug.py
@@ -2,4 +2,7 @@ from fastai.vision.utils import *
 from pathlib import Path
 
 path = Path("images")
-resize_images(path, dest=path) 
+result = resize_images(path, dest=path, max_size=400) 
+print(result)
+result = resize_images(path, dest=path, max_size=400) 
+print(result)
